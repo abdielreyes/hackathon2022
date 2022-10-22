850f4edf069e9663bb95d8a257013db4d3c1a51e
Josefina Tovar <josefina10969@gmail.com>
2022-10-22 01:09:32 -0500

    Add Title and Subtitle components

--- app/src/components/Title.tsx
+++ app/src/components/Title.tsx
@@ -6,7 +6,7 @@ interface TitleProps {
 
 export default function Title({label}:TitleProps) {
   return (
-    <div>
+    <div className='text-white-2 text-xl text-center py-3'>
       {label}
     </div>
   )
--- app/src/pages/Collection.tsx
+++ app/src/pages/Collection.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Subtitle from '../components/Subtitle'
+import Title from '../components/Title'
 
 export default function Collection() {
 	return (
-		<div>
+		<div className='bg-bbva-blue-1'>
 			Collection
+			<Title label='Mi colección' />
+			<Subtitle label='test' />
 		</div>
 	)
 }

a1030f22ebb6bf29657aa2ec75ef3e68541af378
Josefina Tovar <josefina10969@gmail.com>
2022-10-22 01:16:05 -0500

    Update Collection

--- app/src/pages/Collection.tsx
+++ app/src/pages/Collection.tsx
@@ -4,10 +4,21 @@ import Title from '../components/Title'
 
 export default function Collection() {
 	return (
-		<div className='bg-bbva-blue-1'>
-			Collection
-			<Title label='Mi colección' />
-			<Subtitle label='test' />
+		<div className='flex flex-col'>
+			<section className='bg-bbva-blue-1'>
+				<div className='text-white-2 text-center'>
+					Hola, Joss
+				</div>
+				<Title label='Mi colección' />
+				<div className='text-white-2'>
+					Ordenar por:
+				</div>
+			</section>
+			<section className='bg-white-1'>
+				<div className='bg-white-2 m-5 drop-shadow-md'>
+				<Subtitle label='Nombre del NFT' />
+				</div>
+			</section>
 		</div>
 	)
 }

1ab9320fb886243bfd2a973784456cd3aa7febf5
Abdiel <abykings1@gmail.com>
2022-10-22 20:05:41 -0500

    a

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,9 @@ app.use(cookieParser())
 dotenv.config()
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
-app.use(cors());
+app.use(cors({
+	credentials:true
+}));
 
 mongoose.connect(process.env.DB_CONNECT,(err)=>{ 
 	if(err){

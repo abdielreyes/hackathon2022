8bd0a9195ff3de26b65105d437c7e8c74c1d5dd2
Josefina Tovar <josefina10969@gmail.com>
2022-10-23 01:57:50 -0500

    Update Collection style

--- app/src/pages/Collection.tsx
+++ app/src/pages/Collection.tsx
@@ -1,37 +1,33 @@
 import React from 'react'
 import NFTCard from '../components/NFTCard'
-import Subtitle from '../components/Subtitle'
 import Title from '../components/Title'
 
 export default function Collection() {
 	return (
 		<div className='flex flex-col'>
 			<section className='bg-bbva-blue-1'>
-				<div className='text-white-2 text-center'>
-					Hola, Joss
-				</div>
 				<Title label='Mi colección' />
-				<div className='text-white-2'>
-					Ordenar por:
-				</div>
 			</section>
-			<section className='bg-white-1 flex flex-wrap'>
+			<section className='bg-white-1 flex flex-wrap ml-3'>
+				<p className='text-gray-1 px-4 pt-4'>
+					Haz click sobre un NFT para ver sus detalles y la promoción incluída.
+				</p>
 				<NFTCard
 					tokenId='1'
 					rarity='common'
-					name='NFT Name'
+					name='NFT Común'
 					src='https://gateway.pinata.cloud/ipfs/QmPR5g6pdVPDzS5ksVtQTxL1nBm1vLtZqjn2Mri13AuxrB'
 				/>
 				<NFTCard
 					tokenId='2'
 					rarity='rare'
-					name='NFT Name'
+					name='NFT Raro'
 					src='https://gateway.pinata.cloud/ipfs/QmPR5g6pdVPDzS5ksVtQTxL1nBm1vLtZqjn2Mri13AuxrB'
 				/>
 				<NFTCard
 					tokenId='3'
 					rarity='epic'
-					name='NFT Name'
+					name='NFT Épico'
 					src='https://gateway.pinata.cloud/ipfs/QmPR5g6pdVPDzS5ksVtQTxL1nBm1vLtZqjn2Mri13AuxrB'
 				/>
 			</section>
